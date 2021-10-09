0bc1681d4955b969ba5e2e99334bb27bd5032ca1
toojey7 <>
2021-10-09 07:22:35 +0000

    code: add quickSort

--- algorithm/Sort/sort_comparation.py
+++ algorithm/Sort/sort_comparation.py
@@ -1,5 +1,6 @@
 from selection_sort import selection_Sort
 from insertion_sort import insertion_Sort
+from quick_sort import quick_Sort
 import time
 
 if __name__ == "__main__":
@@ -9,10 +10,10 @@ if __name__ == "__main__":
     n=int(file.readline())
     data=list(map(int,file.readline().split()))
 
-  functions=[selection_Sort,insertion_Sort]  
+  functions=[selection_Sort,insertion_Sort,quick_Sort]  
   for function in functions:
     start_time=time.time()
     function(n,data,0,n-1)
     end_time=time.time()
     print(function)
-    print("sort time: "+ str((end_time-start_time)*1000)+" seconds")
+    print("sort time: "+ str((end_time-start_time))+" seconds")
