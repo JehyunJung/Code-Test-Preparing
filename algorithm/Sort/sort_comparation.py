36fc36835f3df2034005e3d261ca0d9cc7fc4aea
toojey7 <>
2021-10-09 04:39:58 +0000

    code: add insertion_Sort

--- algorithm/Sort/sort_comparation.py
+++ algorithm/Sort/sort_comparation.py
@@ -1,15 +1,18 @@
 from selection_sort import selection_Sort
+from insertion_sort import insertion_Sort
 import time
 
 if __name__ == "__main__":
   n=0
   data=[]
   with open("data.txt","r") as file:
-    n=file.readline()
+    n=int(file.readline())
     data=list(map(int,file.readline().split()))
 
-  
-  start_time=time.time()
-  selection_Sort(n,data)
-  end_time=time.time()
-  print("selection sort time: "+ end_time-start_time+"seconds")
+  functions=[selection_Sort,insertion_Sort]  
+  for function in functions:
+    start_time=time.time()
+    function(n,data)
+    end_time=time.time()
+    print(function)
+    print("sort time: "+ str((end_time-start_time)*1000)+" seconds")
