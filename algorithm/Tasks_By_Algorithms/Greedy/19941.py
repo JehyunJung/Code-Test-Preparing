e1931759d203fdd9f848ab022f7811e889ebd574
jehyunJung <toojey7@gmail.com>
2023-04-27 00:55:48 +0900

    code: Task Solutions 230426

---
+++ algorithm/Tasks_By_Algorithms/Greedy/19941.py
@@ -0,0 +1,30 @@
+import sys
+def solution():
+    eat_count=0
+
+    visited=[False]*n
+
+    for i in range(n):
+        if datas[i] != "P":
+            continue
+        for dx in range(-k,k+1):
+            print(dx)
+            adj_index=i+dx
+
+            if adj_index < 0 or adj_index >= n:
+                continue
+
+            if datas[adj_index] == "H" and not visited[adj_index]:
+                eat_count+=1
+                visited[adj_index]=True
+                break
+        
+    print(eat_count)
+
+if __name__ == "__main__":
+    sys.stdin=open("input19941.txt","r")
+    n,k=map(int,input().split())
+    datas=list(input())
+    solution()
+        
+
